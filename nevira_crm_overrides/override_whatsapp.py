dfafb069f3049b275698a0bc1b633fbc4d821777
FRANKSADWAR <billyfranks98@gmail.com>
2026-09-22 02:10:21 +0000

    run before insert first

--- nevira_crm_overrides/override_whatsapp.py
+++ nevira_crm_overrides/override_whatsapp.py
@@ -7,17 +7,20 @@ from frappe_whatsapp.utils import format_number
 
 class CustomWhatsAppMessage(WhatsAppMessage):
 
+    def before_insert(self):
+        self.set_to_from_crm_lead()
+        super().before_insert()
+
     def before_save(self):
         self.set_to_from_crm_lead() 
 
     def set_to_from_crm_lead(self):
-
         ## Only act when a lead is selected
         if not self.get("custom_crm_lead"):
             return
 
         mobile_no = frappe.db.get_value("CRM Lead", self.custom_crm_lead, "mobile_no")
-        frappe.log(f"Mobile number is {mobile_no}")
+        frappe.msgprint(f"Fetched mobile no: {mobile_no}", alert=True, indicator="blue")
         if mobile_no:
             self.to = mobile_no
         
